b679a43b943335ace3fb49d378c713a17e2505f8
Cristiano Rocha <cristianojedi@gmail.com>
2019-03-20 01:03:23 -0300

    views

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,37 +1,67 @@
 class VideosController < ApplicationController
-  before_action :set_video, only: %i[show update destroy]
+  before_action :set_video, only: %i[show update edit destroy]
   skip_before_action :verify_authenticity_token
 
   def index
+    # @videos = Video.all
+    # render json: @videos, status: :ok
     @videos = Video.all
-    render json: @videos, status: :ok
   end
 
   def list
     @videos = current_user.videos
-    render json: @videos, status: :ok
+    # render json: @videos, status: :ok
   end
 
   def show
-    render json: @video, status: :ok
+    # render json: @video, status: :ok
+    # @video = Video.find(params[:id])
+  end
+
+  def new
+    @video = Video.new
+  end
+
+  def edit
+    # @video = Video.find_by(id: params[:id], user_id: current_user.id)
   end
 
   def create
     @video = Video.new(video_params.merge(user: current_user))
 
-    if @video.save
-      render json: @video, status: :created
-    else
-      render json: @video.errors, status: :unprocessable_entity
+    respond_to do |format|
+      if @video.save
+        format.html { redirect_to @video, notice: 'Video cadastrado com sucesso.' }
+        format.json { render :show, status: :created, location: @video }
+      else
+        format.html { render :new }
+        format.json { render json: @video.errors, status: :unprocessable_entity }
+      end
     end
+    # @video = Video.new(video_params.merge(user: current_user))
+
+    # if @video.save
+    #   render json: @video, status: :created
+    # else
+    #   render json: @video.errors, status: :unprocessable_entity
+    # end
   end
 
   def update
-    if @video.update(video_params.merge(user: current_user))
-      render json: @video, status: :ok
-    else
-      render json: @video.errors, status: :unoprocessable_entity
+    respond_to do |format|
+      if @video.update(video_params)
+        format.html { redirect_to action: :show, notice: 'Video alterado com sucesso.' }
+        format.json { render :show, status: :ok, location: @video }
+      else
+        format.html { render :edit }
+        format.json { render json: @video.errors, status: :unprocessable_entity }
+      end
     end
+    # if @video.update(video_params.merge(user: current_user))
+    #   render json: @video, status: :ok
+    # else
+    #   render json: @video.errors, status: :unoprocessable_entity
+    # end
   end
 
   def destroy
@@ -42,7 +72,8 @@ class VideosController < ApplicationController
   private
 
   def set_video
-    @video = current_user.videos.where(id: params[:id])
+    # @video = current_user.videos.where(id: params[:id])
+    @video = Video.find_by(id: params[:id], user_id: current_user.id)
   end
 
   def video_params
